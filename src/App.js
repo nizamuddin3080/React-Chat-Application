7e130cf2e4675b45dc8b68768e64ee2fb66870b5
nizamuddin3080 <mdnizamuddin3080@gmail.com>
2021-03-20 10:54:31 +0600

    Demo Chat render successful

--- src/App.js
+++ src/App.js
@@ -2,3 +2,15 @@ import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
 import './app.css';
 
+const App = () => {
+	return (
+		<ChatEngine
+            height='100vh'
+			projectID='2fe36ca1-dd1d-4c36-8874-c7f658e22b12'
+			userName='demo'
+			userSecret='123456'
+		/>
+	);
+}
+
+export default App;
