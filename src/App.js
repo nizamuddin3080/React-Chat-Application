5f38f2224039c84deca101bf03578f865928eac4
nizamuddin3080 <mdnizamuddin3080@gmail.com>
2021-03-20 18:57:31 +0600

    Add Some Logic on the based of user activity

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 import './app.css';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
 			projectID='2fe36ca1-dd1d-4c36-8874-c7f658e22b12'
 			userName='demo'
 			userSecret='123456'
+			renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
 		/>
 	);
 }
